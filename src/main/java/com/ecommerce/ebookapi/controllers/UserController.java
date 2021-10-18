2e5b51efb1d2f5c18324d6105d2dc8038fe5f0fc
Enrique Inca <inca.quispe.e@outlook.com>
2021-10-17 23:08:58 -0500

    Add crud User and Book

--- src/main/java/com/ecommerce/ebookapi/controllers/UserController.java
+++ src/main/java/com/ecommerce/ebookapi/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.ecommerce.ebookapi.controllers;
 
-import com.ecommerce.ebookapi.models.Book;
-import com.ecommerce.ebookapi.repositories.IBookRepository;
+import com.ecommerce.ebookapi.models.User;
+import com.ecommerce.ebookapi.repositories.IUserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,38 +12,40 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
-public class BookController {
+public class UserController {
+    private final IUserRepository repository;
 
-    private final IBookRepository repository;
-
-    BookController(IBookRepository repository) {
+    UserController(IUserRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/books")
-    public ResponseEntity<List<Book>> getAll() {
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAll() {
         return ResponseEntity.ok(this.repository.findAll());
     }
 
-    @PostMapping("/books")
-    public ResponseEntity add(@RequestBody Book book) {
-        this.repository.save(book);
+    @PostMapping("/users")
+    public ResponseEntity add(@RequestBody User user) {
+        this.repository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PutMapping(value = "/books/{id}")
-    public ResponseEntity<Book> update(@PathVariable("id") String id, @RequestBody Book book) {
+    @PutMapping(value = "/users/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") String id, @RequestBody User user) {
         return this.repository.findById(id)
                 .map(data -> {
-                    data.setTitle(book.getTitle());
-                    data.setPrice(book.getPrice());
+                    data.setEmail(user.getEmail());
+                    data.setFirstName(user.getFirstName());
+                    data.setLastName(user.getLastName());
+                    data.setMiddleName(user.getMiddleName());
+                    data.setPassword(user.getPassword());
                     data.setUpdateAt(LocalDateTime.now());
-                    Book update = this.repository.save(data);
+                    User update = this.repository.save(data);
                     return ResponseEntity.ok().body(update);
                 }).orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping(value = "/books/{id}")
+    @DeleteMapping(value = "/users/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") String id) {
         return this.repository.findById(id)
                 .map(data -> {
@@ -51,6 +53,5 @@ public class BookController {
                     return ResponseEntity.ok().build();
                 }).orElse(ResponseEntity.notFound().build());
     }
-
 
 }
--- src/main/java/com/ecommerce/ebookapi/repositories/IUserRepository.java
+++ src/main/java/com/ecommerce/ebookapi/repositories/IUserRepository.java
@@ -1,9 +1,9 @@
 package com.ecommerce.ebookapi.repositories;
 
-import com.ecommerce.ebookapi.models.Book;
+import com.ecommerce.ebookapi.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBookRepository extends MongoRepository<Book,String> {
+public interface IUserRepository extends MongoRepository<User,String> {
 }
